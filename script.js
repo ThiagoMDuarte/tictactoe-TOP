7744800ef4f6d26ab0e794b4e88bb8075a351a92
Thiago Duarte <104696554+ThiagoMDuarte@users.noreply.github.com>
2024-10-28 15:51:33 -0300

    Create function CreatePlayer and initialize Player

--- script.js
+++ script.js
@@ -1,17 +1,27 @@
 // FUNÇÃO createPlayer() - CRIAR JOGADORES
-
+function createPlayer(name,symbol) {
+    return {name,symbol}
+}
 // FUNÇÃO TicTacToeGame() - PRINCIPAL
+function TicTacToeGame () {
+    let board = Array(9).fill(null);
+    let players = [];
+    let currentPlayer;
+    // FUNÇÃO initializePlayer() - CHAMA O CREATE PLAYER
+    const initializePlayer = () => {
+        players = [createPlayer('Player1','X'),createPlayer('Player2','O')]
+        currentPlayer = players[0]
+    };
+    // FUNÇÃO switchPlayer() -  ALTERNA ENTRE P1 E P2
 
-// FUNÇÃO initializePlayer() - CHAMA O CREATE PLAYER
+    // FUNÇÃO makeMove() - FAZ A JOGADA E CHAMA switchPlayer
 
-// FUNÇÃO switchPlayer() -  ALTERNA ENTRE P1 E P2
+    // FUNÇÃO checkWinOrDraw() - VERIFICAR VITORIA / EMPATE **
 
-// FUNÇÃO makeMove() - FAZ A JOGADA E CHAMA switchPlayer
+    // FUNÇÃO displayBoard() - EXIBIR TABULEIRO NO DOM
 
-// FUNÇÃO checkWinOrDraw() - VERIFICAR VITORIA / EMPATE **
+    // FUNÇÃO updateBoard() - MODIFICAR O DOM
 
-// FUNÇÃO displayBoard() - EXIBIR TABULEIRO NO DOM
-
-// FUNÇÃO updateBoard() - MODIFICAR O DOM
-
-// FUNÇÃO resetGame - REINICIAR O JOGO 
+    // FUNÇÃO resetGame - REINICIAR O JOGO 
+}
+    
